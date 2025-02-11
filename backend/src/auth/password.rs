dd2b091a1007b7548894a51427b9121e95dbda43
jgoudiab <jeangabrielgoudiaby@gmail.com>
2025-02-11 17:26:53 +0000

    Init project

--- backend/src/auth/password.rs
+++
@@ -1,9 +0,0 @@
-use bcrypt::{hash, verify, DEFAULT_COST};
-
-pub fn hash_password(password: &str) -> String {
-    hash(password, DEFAULT_COST).expect("Password hashing failed")
-}
-
-pub fn verify_password(password: &str, hashed: &str) -> bool {
-    verify(password, hashed).expect("Password verification failed")
-}
